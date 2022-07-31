ff769a83d50fb1fcc91ad690e6dca54cddf4b804
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-07-31 15:59:24 -0300

    Add files class 1

---
+++ 01/01.py
@@ -0,0 +1,28 @@
+#print("Hello World!\nHello World!\nHello World")
+# print (10+20)
+# print("Hello" + "Juliana")
+# print("Hello" +" "+ "Juliana")
+
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+
+#print("Hello " + input("What's your name?"))
+
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+
+    
---
+++ 01/aula1_Ex_1.py
@@ -0,0 +1,6 @@
+name= input("What's your name? ")
+print(len(name))
+
+
+# print("Juliana")
+# print(len("Juliana"))
