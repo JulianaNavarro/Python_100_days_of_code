7339a0d39dc80de691532b68778452ac7a7d33f5
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-08-02 23:52:53 -0300

    .

--- 02/aula2_Ex_2.py
+++ 02/aula2_Ex_2.py
@@ -12,6 +12,18 @@
 # Adição
 # Subtração
 
-print(3 * (3 + 3) / 3 - 3)
+#print(3 * (3 + 3) / 3 - 3)
 
 
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+height = float(height)
+weight = int(weight)
+
+BMI = weight / height ** 2
+print(BMI)
