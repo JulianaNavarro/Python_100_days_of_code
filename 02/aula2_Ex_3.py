d8467b8d330f6591d337c71287554d407e3b0649
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-08-04 08:21:57 -0300

    class Ex 2

--- 02/aula2_Ex_3.py
+++ 02/aula2_Ex_3.py
@@ -9,9 +9,14 @@ age = input("What is your current age?\n")
 #Write your code below this line 👇
 age=int(age)
 
+months= age * 12
 weeks= age * 52
 days=  age * 365
-months_left= (90-age)*12
 
-print(f"You have {days} days, {weeks} weeks, and {months_left} months left until your 90s. ")
+months_left= (90-age)*12
+days_left= (90-age) * 365
+weeks_left= (90-age)* 52
+
+print(f"You lived {days} days, {weeks} weeks, and {months} months")
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left until your 90s. ")
 
