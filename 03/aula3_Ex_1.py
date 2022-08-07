d2ae43056e5f59de3b41b1d5604020a5703e2178
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-08-06 21:32:14 -0300

    class 3

---
+++ 03/aula3_Ex_1.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? \n"))
+
+if  height >= 120:
+    print("You can ride to the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
