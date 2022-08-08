f852ba1f810dd3f872a42a2786892bea03adeb4f
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-08-08 19:25:25 -0300

    class 2 ex

--- 03/aula3_Ex_1.py
+++ 03/aula3_Ex_1.py
@@ -3,5 +3,16 @@ height = int(input("What is your height in cm? \n"))
 
 if  height >= 120:
     print("You can ride to the rollercoaster!")
+
+    age = int(input("What is your age?\n"))
+    if age < 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    elif age >= 65:
+       print("Please pay $5.") 
+    else:
+        print("Please pay $12.")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
