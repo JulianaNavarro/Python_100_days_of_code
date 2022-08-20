1fe4dcbb20a7204e491b7593e79bf56bef14c3e4
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-08-20 13:08:19 -0300

    class 3_Pizza

--- 03/aula3_Ex_1.py
+++ 03/aula3_Ex_1.py
@@ -1,18 +1,30 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? \n"))
 
+bill = 0
 if  height >= 120:
     print("You can ride to the rollercoaster!")
 
     age = int(input("What is your age?\n"))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     elif age >= 65:
-       print("Please pay $5.") 
+        bill = 5
+        print("Elderly tickts are $5.") 
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+    wants_photo = input("Do you want a photo taken? Y or N.\n").upper()
+    # wants_photo = wants_photo.upper()
+    # print(wants_photo)
+    if wants_photo == "Y":
+        # bill = bill + 3
+        bill += 3
+    print(f"Your final bill is ${bill}")
 
 else:
     print("Sorry, you have to grow taller before you can ride.")
---
+++ 03/aula3_pizza.py
@@ -0,0 +1,41 @@
+# 🚨 Don't change the code below 👇
+from cgi import print_arguments
+
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L \n").upper()
+add_pepperoni = input("Do you want pepperoni? Y or N \n").upper()
+extra_cheese = input("Do you want extra cheese? Y or N \n").upper()
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+price = 0
+
+if extra_cheese == "Y":
+     price += 1
+     
+if size == "S":
+    price += 15
+    if add_pepperoni == "Y":
+       price += 2
+
+elif size == "M":
+    price += 20
+    if add_pepperoni == "Y":
+       price += 3
+else:
+    price += 25
+    # print(price)
+    if add_pepperoni == "Y":
+       price += 3
+    #    print(price)
+
+# print(price)
+
+print(f"Your final bill is: ${price}")
+
+# print(size)
+# print(add_pepperoni)
+# print(extra_cheese)
+
