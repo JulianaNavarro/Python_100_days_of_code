8fa42f4ef28996a1782906dc68a2f0f8faf6305c
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-08-26 07:58:07 -0300

    class 4 Game

---
+++ 04/aula_4_luck.py
@@ -0,0 +1,10 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+random_game = random.randint(1,0)
+if random_game == 1:
+    print("Heads")
+else:
+        print("Tails")
